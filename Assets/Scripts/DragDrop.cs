1d4a563bd91301f0fe9ef217a40d7e4f1c94e032
eevrip <elen.evri9@gmail.com>
2024-09-08 12:21:18 +0200

    First Submission

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragDrop : MonoBehaviour
+{
+   
+    private Camera cam;
+    private Vector3 offset;
+    //private PuzzlePiece tile;
+    
+    private void OnMouseDown()
+    {
+      
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            
+            return;
+        }
+       // PuzzleManager.Sorting.BringToFront(tile.SpRenderer);
+        Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        offset = transform.position - mousePos;
+        Debug.Log("Mousedown");
+    }
+    private void OnMouseDrag()
+    {
+
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+       
+        //transform.position = currPos;
+       
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        Vector3 currPos = cam.ScreenToWorldPoint(mousePos) + offset;
+        //tile.UpdatePosition(currPos);
+        transform.position = currPos;
+       // Debug.Log("Mousedrag");
+    }
+    private void OnMouseUp()
+    {
+
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        //tile.IsCorrectPositionNeighbours();
+        //if (tile.IsCorrectPosition())
+       // {
+       //     Debug.Log("Correct " + tile.TileID);
+       // }
+    }
+    private void OnMouseOver()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        if(Input.GetMouseButtonDown(1)) {
+            //transform.Rotate(Vector3.back, 90f);
+          //  PuzzleManager.Sorting.BringToFront(tile.SpRenderer);
+           // tile.UpdateRotation();
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+      //  tile = GetComponent<PuzzlePiece>();
+
+    }
+
+    
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    #region Singleton
+    public static MenuManager instance;
+  
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    #endregion 
+    public static bool isGamePaused = false;
+    public Animator transitionLevel;
+    
+    public Animator transitionPauseMenu;
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // StartCoroutine(LoadingLevel(SceneManager.GetActiveScene().buildIndex+1));
+        StartCoroutine(LoadingLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void ContinueGame()
+    { //Time.timeScale = 1f;
+        if (transitionPauseMenu)
+            transitionPauseMenu.SetTrigger("continue");
+        Debug.Log("continue");
+       
+        isGamePaused = false;
+    }
+    public void PauseGame()
+    {
+        if (transitionPauseMenu)
+        {
+            transitionPauseMenu.SetTrigger("pause");
+            Debug.Log("pause");
+            StartCoroutine(LoadPauseMenu());
+        }
+       
+    }
+    
+    public void MainMenu()
+    {
+        // ContinueGame();
+        // SceneManager.LoadScene(0);
+        isGamePaused = false;
+        StartCoroutine(LoadingLevel(0));
+    }
+    public void Credits()
+    {
+        
+        
+        StartCoroutine(LoadingLevel(2));
+    }
+    IEnumerator LoadPauseMenu()
+    {
+        yield return new WaitForSeconds(1f); 
+        //Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+    IEnumerator LoadingLevel(int levelIndx)
+    {
+       
+       
+        if(transitionLevel)
+            transitionLevel.SetTrigger("LoadNext");
+       
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene(levelIndx);
+    }
+    
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isGamePaused)
+            {
+
+                PauseGame();
+            }
+            else
+            {
+
+                ContinueGame();
+            }
+        }
+    }
+
+}
